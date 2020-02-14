06ecbb961393060cd9e614d6109f93a5fe1d7959
AitorKimobox <aburruezo.kimobox@gmail.com>
2020-02-14 18:27:13 +0100

    hola

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        // A empresa
+        // B trabajador
+        // C portatil
+
+
+
         System.out.println("hola");
     }
 
