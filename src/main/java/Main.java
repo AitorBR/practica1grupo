374391045965da4e9cc16feb4ce3b21e8b7fc9f5
AitorKimobox <aburruezo.kimobox@gmail.com>
2020-02-24 21:37:41 +0100

    jajasalu2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
-import Controlador.EmpleadoDao;
-import Controlador.EmpleadoDaoImp;
+import Controlador.*;
 import Model.Empleado;
+import Model.Empresa;
+import Model.Portatil;
 import Vista.View;
 
 public class Main {
@@ -13,18 +14,43 @@ public class Main {
         // B trabajador
         // C portatil
 
-        Empleado empleado = new Empleado("1", "asdasd", "masdasdasd", 15);
-        Empleado empleado2 = new Empleado("2", "asdasdasdasd", "masdasdasdasdas", 17);
+        Empresa empresa = new Empresa("1", "1", 154, "15");
+        Empresa empresa2 = new Empresa("1", "1", 154, "15");
 
+        Empleado empleado = new Empleado("1", "asdasd", "masdasdasd", 15);
+        Empleado empleado2 = new Empleado("1", "asdasdasdasd", "masdasdasdasdas", 17);
+
+        Portatil portatil = new Portatil("1", "asdasd", "1", 15);
+        Portatil portatil2 = new Portatil("1", "asdasd", "1", 15);
+
+        EmpresaDao empresaDao = new EmpresaDaoImp();
         EmpleadoDao empleadoDao = new EmpleadoDaoImp();
+        PortatilDao portatilDao = new PortatilDaoImp();
+
+        empresaDao.crear(empresa);
+        empresaDao.mostrar();
+        empresaDao.actualizar(empresa2, "1");
+        empresaDao.mostrar();
+        empresaDao.eliminar("1");
+        empresaDao.eliminar("1");
+        empresaDao.mostrar();
 
         empleadoDao.crear(empleado);
         empleadoDao.mostrar();
         empleadoDao.actualizar(empleado2, "1");
         empleadoDao.mostrar();
         empleadoDao.eliminar("1");
-        empleadoDao.eliminar("2");
+        empleadoDao.eliminar("1");
         empleadoDao.mostrar();
+
+        portatilDao.crear(portatil);
+        portatilDao.mostrar();
+        portatilDao.actualizar(portatil2, "1");
+        portatilDao.mostrar();
+        portatilDao.eliminar("1");
+        portatilDao.eliminar("1");
+        portatilDao.mostrar();
+
 
         System.out.println("hola");
         System.out.println("Adios");
