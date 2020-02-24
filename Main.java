c8b1d3dda1dcbb049032d4493513073c2bafcf2e
DavidSanchezMenendez <55991736+DavidSanchezMenendez@users.noreply.github.com>
2020-02-24 16:43:34 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // A empresa
+        // B trabajador
+        // C portatil
+
+
+        System.out.println("hola");
+        System.out.println("Adios");
+    }
+
+
+}
